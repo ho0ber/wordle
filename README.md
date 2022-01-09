a11f253385d999a179754ca4aedda856ee3c051c
Samuel Colburn <ho0ber@gmail.com>
2022-01-09 13:38:42 -0500

    interactive guesser

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # wordle
 Wordle solving helper
+
+Best guess words:
+1) arose
+2) until
--- wordle.py
+++ wordle.py
@@ -10,36 +10,36 @@ class Wordle(object):
     
     def refine(self, guess, match="_____"):
         self.guessed_letters.update(c for c in guess)
-        non = [c for c in guess if c not in match.lower()]
+        non = set(c for c in guess if c not in match.lower())
         self.words = [w for w in self.words if self.word_matches(w, match, non)]
 
     def word_matches(self, word, match, non):
         for i in range(5):
-            if match[i] in "_ ":
-                continue
-            if match[i].lower()==match[i]:
-                if match[i] == word[i] or match[i] not in word:
+            if not match[i] in "_ ":
+                if match[i].lower() == match[i]:
+                    if match[i] == word[i] or match[i] not in word:
+                        return False
+                elif match[i].lower() != word[i]:
                     return False
-            else:
-                if match[i].lower() != word[i]:
-                    return False
-        for c in word:
-            if c in non:
-                return False
-        return True
-    
-    def display(self):
+
+        return non.isdisjoint(word)
+
+    def recommend(self):
         counts = Counter(c for w in self.words for c in w if c not in self.guessed_letters)
-        if counts:
-            print("Common new letters: "+" ".join([f"{l}({c})" for l,c in counts.most_common()]))
-        commons = [(word, sum(counts[c] for c in list(set(word)))) for word in self.words]
-        commons.sort(key=lambda x: x[1], reverse=True)
-        for word,score in commons:
-            print(f"{word} ({score})")
+        scores = [(word, sum(counts[c] for c in list(set(word)))) for word in self.words]
+        scores.sort(key=lambda x: x[1], reverse=True)
+        recommendation = scores[0][0]
+        print(f"Recommended: {recommendation}")
+        return recommendation
+
+    def interactive_solve(self):
+        while True:
+            rec = wordle.recommend()
+            result = input()
+            if result.upper() == result and set("_ ").isdisjoint(result):
+                break
+            wordle.refine(rec, result)
 
 if __name__ == "__main__":
     wordle = Wordle("words_en.txt")
-    wordle.refine("arose", "_ro_E")
-    wordle.refine("until")
-    wordle.refine("forge", "_ORGE")
-    wordle.display()
+    wordle.interactive_solve()
